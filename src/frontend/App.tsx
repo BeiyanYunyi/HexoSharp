bc7a10736f01f44af57e17c34418d64350aad3d6
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-02-17 23:02:34 +0800

    🐛 Fix filename #2

---
+++ src/frontend/App.tsx
@@ -0,0 +1,22 @@
+import { Container, Typography } from '@mui/material';
+import React from 'react';
+import octokit from './service/octokit';
+
+const App = () => {
+  const [data, setData] = React.useState<string>('');
+  React.useEffect(() => {
+    octokit.repos
+      .get({ owner: 'lixiang810', repo: 'HexoSharp' })
+      .then((res) => {
+        setData(JSON.stringify(res.data, null, 2));
+      })
+      .catch((e) => console.log(e));
+  }, []);
+  return (
+    <Container>
+      <Typography sx={{ whiteSpace: 'pre' }}>{data}</Typography>
+    </Container>
+  );
+};
+
+export default App;
