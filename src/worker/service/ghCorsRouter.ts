cf713475cd3884fd82c0dd3345158fe312ca810d
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-02-22 12:35:29 +0800

    🐛 Handle 304 correctly

--- src/worker/service/ghCorsRouter.ts
+++ src/worker/service/ghCorsRouter.ts
@@ -24,48 +24,53 @@ const fix = (myHeadersFixing: Headers, event: FetchEvent) => {
 
 const ghCorsRouter = () => {
   router.all('/api/gh/*', async (req, event: FetchEvent) => {
-    try {
-      const isOptions = event.request.method === 'OPTIONS';
-      const targetUrl = req.url.replace(/https?.*gh\//, '');
-      if (!targetUrl.includes('github')) {
-        return new Response('Forbidden', { status: 403, statusText: 'Forbidden' });
-      }
-      const recvHeaders: Record<string, string> = {};
-      // eslint-disable-next-line no-restricted-syntax
-      for (const pair of event.request.headers.entries()) {
-        if (
-          pair[0].match('^origin') == null &&
-          pair[0].match('eferer') == null &&
-          pair[0].match('^cf-') == null &&
-          pair[0].match('^x-forw') == null &&
-          pair[0].match('^x-cors-headers') == null
-        )
-          // eslint-disable-next-line prefer-destructuring
-          recvHeaders[pair[0]] = pair[1];
-      }
-      const newReq = new Request(event.request, { headers: recvHeaders });
-      const fetchUrl = decodeURIComponent(targetUrl);
-      const res = await fetch(fetchUrl, newReq);
-      const resHeaders = fix(new Headers(res.headers), event);
-      const body = isOptions ? null : await res.arrayBuffer();
-      const corsHeaders: string[] = [];
-      const allh: Record<string, string> = {};
-      // eslint-disable-next-line no-restricted-syntax
-      for (const pair of res.headers.entries()) {
-        corsHeaders.push(pair[0]);
+    const isOptions = event.request.method === 'OPTIONS';
+    const targetUrl = req.url.replace(/https?.*gh\//, '');
+    if (!targetUrl.includes('github')) {
+      return new Response('Forbidden', { status: 403, statusText: 'Forbidden' });
+    }
+    const recvHeaders: Record<string, string> = {};
+    // eslint-disable-next-line no-restricted-syntax
+    for (const pair of event.request.headers.entries()) {
+      if (
+        pair[0].match('^origin') == null &&
+        pair[0].match('eferer') == null &&
+        pair[0].match('^cf-') == null &&
+        pair[0].match('^x-forw') == null &&
+        pair[0].match('^x-cors-headers') == null
+      )
         // eslint-disable-next-line prefer-destructuring
-        allh[pair[0]] = pair[1];
-      }
-      corsHeaders.push('cors-received-headers');
-      resHeaders.set('Access-Control-Expose-Headers', corsHeaders.join(','));
-      resHeaders.set('cors-received-headers', JSON.stringify(allh));
-      return new Response(res.status <= 400 ? body : null, {
+        recvHeaders[pair[0]] = pair[1];
+    }
+    const newReq = new Request(event.request, { headers: recvHeaders });
+    const fetchUrl = decodeURIComponent(targetUrl);
+    const res = await fetch(fetchUrl, newReq);
+    const resHeaders = fix(new Headers(res.headers), event);
+    const is304 = res.status === 304;
+    const body = isOptions || is304 ? null : await res.arrayBuffer();
+    const corsHeaders: string[] = [];
+    const allh: Record<string, string> = {};
+    // eslint-disable-next-line no-restricted-syntax
+    for (const pair of res.headers.entries()) {
+      corsHeaders.push(pair[0]);
+      // eslint-disable-next-line prefer-destructuring
+      allh[pair[0]] = pair[1];
+    }
+    corsHeaders.push('cors-received-headers');
+    resHeaders.set('Access-Control-Expose-Headers', corsHeaders.join(','));
+    resHeaders.set('cors-received-headers', JSON.stringify(allh));
+    try {
+      return new Response(body, {
         headers: resHeaders,
         status: isOptions ? 200 : res.status,
         statusText: isOptions ? 'OK' : res.statusText,
       });
     } catch (e) {
-      console.error(e);
+      console.error({
+        body: body ? body.toString() : null,
+        status: isOptions ? 200 : res.status,
+        statusText: isOptions ? 'OK' : res.statusText,
+      });
       return new Response(JSON.stringify(e), {
         headers: { 'Content-Type': 'application/json' },
         status: 500,
