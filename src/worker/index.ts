b41a925b9cd340fba6ad8d76011216b0d18ed478
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-04-08 15:33:57 +0800

    🔒️ Store password hash instead of password itself

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,5 +1,11 @@
 /* eslint-disable no-restricted-globals */
+import { dcodeIO } from 'bcryptjs';
 import handleRequest from './handler';
+
+// 在此处初始化 bcrypt 的随机数算法
+dcodeIO.bcrypt.setRandomFallback((num: number) =>
+  Array.from(crypto.getRandomValues(new Int32Array(num))),
+);
 
 addEventListener('fetch', (event: FetchEvent) => {
   event.respondWith(handleRequest(event.request, event));
--- src/worker/service/authRouter.ts
+++ src/worker/service/authRouter.ts
@@ -1,4 +1,5 @@
 import jwt from '@tsndr/cloudflare-worker-jwt';
+import { dcodeIO } from 'bcryptjs';
 import router from '../router';
 
 /** 提供用户验证服务 */
@@ -8,7 +9,7 @@ const authRouter = () => {
     const savedPassword = await HSPKV.get('password');
     const objToSign = { exp: Math.floor(Date.now() / 1000 + 60 * 60 * 24 * 7) };
     if (savedPassword) {
-      const res = req.parsedJson.password === savedPassword;
+      const res = await dcodeIO.bcrypt.compare(req.parsedJson.password, savedPassword);
       if (!res) return new Response(null, { status: 401 });
       const signed = await jwt.sign(objToSign, JWT_SECRET);
       return new Response(JSON.stringify({ signed }), {
@@ -16,7 +17,8 @@ const authRouter = () => {
         status: 200,
       });
     }
-    await HSPKV.put('password', req.parsedJson.password);
+    const passwordHash = await dcodeIO.bcrypt.hash(req.parsedJson.password, 10);
+    await HSPKV.put('password', passwordHash);
     const signed = await jwt.sign(objToSign, JWT_SECRET);
     return new Response(JSON.stringify({ signed }), {
       headers: { 'Content-Type': 'application/json' },
