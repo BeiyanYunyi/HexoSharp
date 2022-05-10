05d7d31cdc90f3ed3b5a2de373de0d2f2ad859bc
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-05-10 21:40:02 +0800

    🔒️ Fix security issue

--- src/worker/utils/hspCrypt.ts
+++ src/worker/utils/hspCrypt.ts
@@ -17,50 +17,51 @@ const getKey = async (password?: string) => {
   const derivation = await crypto.subtle.deriveBits(
     { name: 'PBKDF2', hash: 'SHA-256', salt: saltAry, iterations: 1048576 },
     importedKey,
-    384,
+    256,
   );
-  const derivedKey = derivation.slice(0, 32);
-  const iv = derivation.slice(-16);
   const importedEncryptionKey = await crypto.subtle.importKey(
     'raw',
-    derivedKey,
+    derivation,
     { name: 'AES-CBC' },
     false,
     ['encrypt', 'decrypt'],
   );
-  return {
-    key: importedEncryptionKey,
-    iv,
-  };
+  return importedEncryptionKey;
 };
 
 /** 加密一段信息
  * @param text - 要被加密的信息
  */
 const encrypt = async (text: string) => {
-  const keyObject = await getKey();
+  const key = await getKey();
   const textEncoder = new TextEncoder();
   const textBuffer = textEncoder.encode(text);
+  const iv = crypto.getRandomValues(new Uint8Array(16));
   const encryptedText: ArrayBuffer = await crypto.subtle.encrypt(
-    { name: 'AES-CBC', iv: keyObject.iv, length: 256 },
-    keyObject.key,
+    { name: 'AES-CBC', iv, length: 256 },
+    key,
     textBuffer,
   );
-  return base65536.encode(new Uint8Array(encryptedText));
+  const encryptedTextAry = new Uint8Array(encryptedText);
+  const aryToEncode = new Uint8Array(iv.length + encryptedTextAry.length);
+  aryToEncode.set(iv);
+  aryToEncode.set(encryptedTextAry, iv.length);
+  return base65536.encode(aryToEncode);
 };
 
 /** 解密一段信息
  * @param cipher - 要被解密的密文
  */
 const decrypt = async (cipher: string) => {
-  const keyObject = await getKey();
+  const key = await getKey();
   const decCipher = base65536.decode(cipher);
-  const textDecoder = new TextDecoder();
+  const iv = decCipher.slice(0, 16);
   const decryptedText = await crypto.subtle.decrypt(
-    { name: 'AES-CBC', iv: keyObject.iv, length: 256 },
-    keyObject.key,
-    decCipher,
+    { name: 'AES-CBC', iv, length: 256 },
+    key,
+    decCipher.slice(16),
   );
+  const textDecoder = new TextDecoder();
   return textDecoder.decode(decryptedText);
 };
 
