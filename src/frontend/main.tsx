973323b4cde7e7644deb23e5817ac8b286b216af
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-05-12 17:52:01 +0800

    🎨 Change import order

--- src/frontend/main.tsx
+++ src/frontend/main.tsx
@@ -1,5 +1,5 @@
-import { createRoot } from 'react-dom/client';
 import React from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const root = createRoot(document.querySelector('div#root')!);
